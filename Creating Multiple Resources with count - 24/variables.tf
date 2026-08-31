d0e810c1be7cb864df6d7dc44e3f63bcdd003157
Aaron Belcher <adotbelcher@gmail.com>
2026-08-30 17:55:52 -0700

    Add validation to EC2 configuration list

--- Creating Multiple Resources with count - 24/variables.tf
+++ Creating Multiple Resources with count - 24/variables.tf
@@ -77,4 +77,22 @@ variable "ec2_instance_config_list" {
   # are provided through a .tfvars file.
 
   default = []
+
+  validation {
+    condition = alltrue([
+      for config in var.ec2_instance_config_list :
+      contains(["t3.micro"], config.instance_type)
+    ])
+
+    error_message = "Only t3.micro instances are allowed."
+  }
+
+  validation {
+    condition = alltrue([
+      for config in var.ec2_instance_config_list :
+      contains(["nginx", "ubuntu"], config.ami)
+    ])
+
+    error_message = "At least one of the provided \"ami\" values is not supported.\nSupported \"ami\" values: \"ubuntu\", \"nginx\"."
+  }
 }
